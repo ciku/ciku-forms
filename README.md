1d4950d3c7098c34dd2a09583c119ed7ac3fbc6a
Saneef Ansari <saneef@gmail.com>
2015-06-22 16:36:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Ciku Forms
+
+Ciku forms is a boilerplate for form elements.
+
+Styling form elements by hand is time consuming. With Ciku Forms with setting a handful of parameters – border color, background color, padding,… — you can get your form elements styled.
+
+## Elements
+
+- [ ] `<button>`
+- [ ] `<datalist>`
+- [ ] `<fieldset>`
+- [ ] `<form>`
+- [ ] `<input>`
+    - [ ] `input[type="color"]`
+    - [ ] `input[type="date"]`
+    - [ ] `input[type="datetime"]`
+    - [ ] `input[type="datetime-local"]`
+    - [ ] `input[type="email"]`
+    - [ ] `input[type="month"]`
+    - [ ] `input[type="number"]`
+    - [ ] `input[type="password"]`
+    - [ ] `input[type="search"]`
+    - [ ] `input[type="tel"]`
+    - [ ] `input[type="text"]`
+    - [ ] `input[type="time"]`
+    - [ ] `input[type="url"]`
+    - [ ] `input[type="week"]` 
+- [ ] `<keygen>`
+- [ ] `<label`
+- [ ] `<legend>`
+- [ ] `<meter>`
+- [ ] `<optgroup>`
+- [ ] `<option>`
+- [ ] `<output>`
+- [ ] `<progress>`
+- [ ] `<select>`
+- [ ] `<textarea>`
+
+## Authors
+- Brajeshwar Oinam (@brajeshwar)[https://twitter.com/brajeshwar]
+- Saneef H. Ansari (@saneef)[https://twitter.com/saneef]
---
+++ gulpfile.babel.js
@@ -0,0 +1,75 @@
+import browserSync from 'browser-sync';
+import del from 'del';
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import runSequence from 'run-sequence';
+import scsslint from 'gulp-scss-lint';
+
+const $ = gulpLoadPlugins();
+
+gulp.task('lint', () => {
+  return gulp.src([
+      './ciku-forms.scss',
+      './settings/**/*.scss',
+      './mixins/**/*.scss',
+      './elements/**/*.scss',
+      './test/**/*.scss'
+    ])
+    .pipe(scsslint({
+      'bundleExec': true
+    }));
+});
+
+gulp.task('clean', () => {
+  del.sync([
+      '.tmp'
+    ]);
+});
+
+gulp.task('sass', () => {
+  return gulp.src('./test/test.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass.sync({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['.']
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer({browsers: ['last 2 version']}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('.tmp/'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('preBuild', (cb) => {
+  return runSequence(
+    'clean',
+    ['sass', 'lint'],
+    cb
+  );
+});
+
+gulp.task('test', ['preBuild'], () => {
+  browserSync({
+    server: {
+      baseDir: ['.tmp', 'test']
+    }
+  });
+
+  gulp.watch([
+      './ciku-forms.scss',
+      './settings/**/*.scss',
+      './mixins/**/*.scss',
+      './elements/**/*.scss',
+      './test/**/*.scss'
+    ], ['sass', 'lint']);
+
+  gulp.watch([
+    'test/**/*',
+    '.tmp/**/*'
+  ]).on('change', browserSync.reload);
+});
+
+gulp.task('default', () => {
+  console.log('Run \`gulp test\` to test');
+});
